fa887f154e354f9518b120fbcd0afa95b4c36995
yanchhuong <yanchhuo_ng@yahoo.com>
2016-11-15 10:04:57 +0900

    test

--- src/main/java/com/heroku/demo/Application.java
+++ src/main/java/com/heroku/demo/Application.java
@@ -16,14 +16,9 @@
 package com.heroku.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+
 @SpringBootApplication
 public class Application {
 
