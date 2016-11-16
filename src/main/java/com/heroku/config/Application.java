98c327825a6326345fb0319e37e1c56f7a505ee6
yanchhuong <yanchhuo_ng@yahoo.com>
2016-11-16 10:53:26 +0900

    test

--- src/main/java/com/heroku/config/Application.java
+++ src/main/java/com/heroku/config/Application.java
@@ -17,13 +17,22 @@ package com.heroku.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+
+
 import org.springframework.boot.Banner;
 
 
 @SpringBootApplication
 @ComponentScan("com.heroku")
-public class Application {
+public class Application extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
     public static void main(String[] args) {
       //  SpringApplication.run(Application.class, args);
