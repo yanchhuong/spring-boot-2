79b2467fe89cc5236fef0747785014fd3875e782
yanchhuong <yanchhuo_ng@yahoo.com>
2016-11-17 15:38:44 +0900

    test

--- src/main/java/com/heroku/model/StorageProperties.java
+++ src/main/java/com/heroku/model/StorageProperties.java
@@ -8,7 +8,7 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private String location = "src/main/resources/templates/img";
+    private String location = "src/main/resources/templates/css";
 
     public String getLocation() {
         return location;
