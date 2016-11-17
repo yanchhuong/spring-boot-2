b7c554ca21fa15397c30877ca6d4cd91c9ba3b28
yanchhuong <yanchhuo_ng@yahoo.com>
2016-11-17 13:18:49 +0900

    test

--- src/main/java/com/heroku/model/StorageProperties.java
+++ src/main/java/com/heroku/model/StorageProperties.java
@@ -2,13 +2,13 @@ package com.heroku.model;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("com.heroku")
+@ConfigurationProperties("com.heroku.storefile")
 public class StorageProperties {
 
     /**
      * Folder location for storing files
      */
-    private String location = "src/main/resources/templates/img";
+    private String location = "src/main/java";
 
     public String getLocation() {
         return location;
