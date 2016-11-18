792e8db3c501de2df8b948c77c49dd6fbee07e4a
yanchhuong <yanchhuo_ng@yahoo.com>
2016-11-18 13:57:20 +0900

    test

--- src/main/webapp/js-lib/upload_file.js
+++ src/main/webapp/js-lib/upload_file.js
@@ -1,46 +1,41 @@
 var _this;
 $(document).ready(function(){
 
- 
-    $("#btfacebooklog").click( "click", function() {
-    	
-    	(function(d, s, id) {
-    	  var js, fjs = d.getElementsByTagName(s)[0];
-    	  if (d.getElementById(id)) return;
-    	  js = d.createElement(s); js.id = id;
-    	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=1585759011719480";
-    	  fjs.parentNode.insertBefore(js, fjs);
-    	}(document, 'script', 'facebook-jssdk'));
-    });
     
-    $("#btlog").click( "click", function() {
-	 
+    $("#upload_file").click( "click", function() {
+	  alert();
+	  ft_UPLOAD_FILE();
    });
 	 
 })
 
-function login(){
+function ft_UPLOAD_FILE(){
 	var csrfHeader = $("meta[name='_csrf_header']").attr("content");
 	var csrfToken = $("meta[name='_csrf']").attr("content")
 	
 	var input={};
 	
-	input["id"]=$("#fid").val();
-	input["username"]=$("#username").val()
-	input["password"]=$("#password").val()
+	var filedata = document.getElementByName("file");
+    formdata = new FormData();
+    var i = 0, len = filedata.files.length, file;
+     for (i; i < len; i++) {
+        file = filedata.files[i];
+        formdata.append("file[]", file);
+    }
+    formdata.append("json",true);
+    
 	  $.ajax({
 	    	type   : 'POST',
-	    	url    : "/login",
+	    	url    : "/file",
 	    	cache: false,
 	        async: false,
-	        
 	        beforeSend: function(xhr) {
 	            xhr.setRequestHeader(csrfHeader, csrfToken);
 	        },
+	        
 	        data:input,
 	    	success :function(result){
 	    	   alert(result);
-	    		
 	    	 }
 		   })
 }
