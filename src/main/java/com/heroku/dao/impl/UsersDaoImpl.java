e8ef34653439dec55b6462c2b6623308d77b8be2
yanchhuong <yanchhuo_ng@yahoo.com>
2016-12-02 14:00:09 +0900

    test

--- src/main/java/com/heroku/dao/impl/UsersDaoImpl.java
+++ src/main/java/com/heroku/dao/impl/UsersDaoImpl.java
@@ -38,7 +38,7 @@ public class UsersDaoImpl extends JdbcDaoSupport implements UsersDao{
 	}
 	@Override
 	public void insertUserDetail(UserDetailBean user) {
-		String sql = "INSERT INTO USER_DETAIL " +"(regdate,username_fk,email,user_cd,) VALUES (?,?,?,?)" ;
+		String sql = "INSERT INTO USER_DETAIL " +"(regdate,username_fk,email,user_cd) VALUES (?,?,?,?)" ;
         this.getJdbcTemplate().update(sql, new Object[]{user.getRegisterDate(),user.getUsername(),user.getEmail(),user.getUserCd()});
 		
 	}
